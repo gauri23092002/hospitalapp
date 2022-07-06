010704d9f8319c31552123b1c9e715d7272bdb70
gauri23092002 <gauri23092002@gmail.com>
2022-07-06 17:57:16 +0530

    searching department

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import { SearchdepartmentComponent } from './searchdepartment/searchdepartment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    SearchdepartmentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchdepartment/searchdepartment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchdepartment',
+  templateUrl: './searchdepartment.component.html',
+  styleUrls: ['./searchdepartment.component.css']
+})
+export class SearchdepartmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
